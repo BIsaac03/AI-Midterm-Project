713523966cd6eb4ef03b69ebcadfc2d7b9af4a10
BIsaac03 <117605942+BIsaac03@users.noreply.github.com>
2023-09-11 19:48:27 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,29 +2,37 @@
 #include <string>
 #include <time.h>
 #include <unistd.h>
-#include <algorithm>
 
 using namespace std;
 
-void shufflePuzzle (char positions[], int &indexOfX, int moves);
-char choose (int indexOfX);
-void printPuzzle(char positions[]);
-void move(char type, char positions[], int &indexOfX);
-int HammingDist(char positions[]);
+void shufflePuzzle (char positions[3][3], int &XindexOfX, int &YindexOfX, int moves);
+char choose (int XindexOfX, int YindexOfX);
+void printPuzzle(char positions[3][3]);
+void move(char type, char positions[3][3], int &XindexOfX, int &YindexOfX);
+int HammingDist(char positions[3][3]);
+int ManhattanDist(char positions[3][3]);
 
 int main()
 {
     srand(time(NULL));
-    char positions[] = {'1', '2', '3', '4', '5', '6', '7', '8', 'X'};
-    int indexOfX = 8;
+    char positions[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', 'X'}};
+    int XindexOfX = 2;
+    int YindexOfX = 2;
     int moves = 0;
 
-    cout << "Before shuffle: " << HammingDist(positions) << endl;
+    cout << endl;
 
-    shufflePuzzle(positions, indexOfX, 1000);
     printPuzzle(positions);
 
-    cout << "After shuffle: " << HammingDist(positions) << endl;
+    cout << "Ham before shuffle: " << HammingDist(positions) << endl;
+    cout << "Man before shuffle: " << ManhattanDist(positions) << endl << endl;
+
+    shufflePuzzle(positions, XindexOfX, YindexOfX, 1000);
+
+    printPuzzle(positions);
+
+    cout << "Ham after shuffle: " << HammingDist(positions) << endl;
+    cout << "Man after shuffle: " << ManhattanDist(positions) << endl << endl;
 
 /*
     cout << "Finding shortest solution..." << endl;
@@ -41,145 +49,188 @@ int main()
 }
 
 // performs 'moves' random moves
-void shufflePuzzle (char positions[], int &indexOfX, int moves)
+void shufflePuzzle (char positions[3][3], int &XindexOfX, int &YindexOfX, int moves)
 {  
     for (int i = 0; i < moves; i++)
     {
-        move(choose(indexOfX), positions, indexOfX);
+        move(choose(XindexOfX, YindexOfX), positions, XindexOfX, YindexOfX);
     }
 }
 
 // chooses a random, valid direction to move
-char choose (int indexOfX)
+ char choose (int XindexOfX, int YindexOfX)
 {
-    if (indexOfX == 0)
+    if (YindexOfX == 0)
     {
-        // returns a random direction (down or right)
-        int choice = rand()%2;
-        if (choice == 0) return 'D';
-        else if (choice == 1) return 'R';
+        if (XindexOfX == 0)
+        {
+            // returns a random direction (down or right)
+            int choice = rand()%2;
+            if (choice == 0) return 'D';
+            else if (choice == 1) return 'R';
+        }
+
+        else if (XindexOfX == 1)
+        {
+            // returns a random direction (down, left, or right)
+            int choice = rand()%3;
+            if (choice == 0) return 'D';
+            else if (choice == 1) return 'L';
+            else if (choice == 2) return 'R';
+        }
+
+        else if (XindexOfX == 2)
+        {
+            // returns a random direction (down or left)
+            int choice = rand()%2;
+            if (choice == 0) return 'D';
+            else if (choice == 1) return 'L';
+        }
     }
 
-    else if (indexOfX == 1)
+    else if (YindexOfX == 1)
     {
-        // returns a random direction (down, left, or right)
-        int choice = rand()%3;
-        if (choice == 0) return 'D';
-        else if (choice == 1) return 'L';
-        else if (choice == 2) return 'R';
+        if (XindexOfX == 0)
+        {
+            // returns a random direction (up, down, or right)
+            int choice = rand()%3;
+            if (choice == 0) return 'U';
+            else if (choice == 1) return 'D';
+            else if (choice == 2) return 'R';
+        }
+
+        else if (XindexOfX == 1)
+        {
+            // returns a random direction (up, down, left, or right)
+            int choice = rand()%4;
+            if (choice == 0) return 'U';
+            else if (choice == 1) return 'D';
+            else if (choice == 2) return 'L';
+            else if (choice == 3) return 'R';
+        }
+
+        else if (XindexOfX == 2)
+        {
+            // returns a random direction (up, down, or left)
+            int choice = rand()%3;
+            if (choice == 0) return 'U';
+            else if (choice == 1) return 'D';
+            else if (choice == 2) return 'L';
+        }
     }
 
-    else if (indexOfX == 2)
+    else if (YindexOfX == 2)
     {
-        // returns a random direction (down or left)
-        int choice = rand()%2;
-        if (choice == 0) return 'D';
-        else if (choice == 1) return 'L';
-    }
+        if (XindexOfX == 0)
+        {
+            // returns a random direction (up or right)
+            int choice = rand()%2;
+            if (choice == 0) return 'U';
+            else if (choice == 1) return 'R';
+        }
 
-    else if (indexOfX == 3)
-    {
-        // returns a random direction (up, down, or right)
-        int choice = rand()%3;
-        if (choice == 0) return 'U';
-        else if (choice == 1) return 'D';
-        else if (choice == 2) return 'R';
-    }
+        else if (XindexOfX == 1)
+        {
+            // returns a random direction (up, left, or right)
+            int choice = rand()%3;
+            if (choice == 0) return 'U';
+            else if (choice == 1) return 'L';
+            else if (choice == 2) return 'R';
+        }
 
-    else if (indexOfX == 4)
-    {
-        // returns a random direction (up, down, left, or right)
-        int choice = rand()%4;
-        if (choice == 0) return 'U';
-        else if (choice == 1) return 'D';
-        else if (choice == 2) return 'L';
-        else if (choice == 3) return 'R';
+        else if (XindexOfX == 2)
+        {
+            // returns a random direction (up or left)
+            int choice = rand()%2;
+            if (choice == 0) return 'U';
+            else if (choice == 1) return 'L';
+        }
     }
-
-    else if (indexOfX == 5)
-    {
-        // returns a random direction (up, down, or left)
-        int choice = rand()%3;
-        if (choice == 0) return 'U';
-        else if (choice == 1) return 'D';
-        else if (choice == 2) return 'L';
-    }
-
-    else if (indexOfX == 6)
-    {
-        // returns a random direction (up or right)
-        int choice = rand()%2;
-        if (choice == 0) return 'U';
-        else if (choice == 1) return 'R';
-    }
-
-    else if (indexOfX == 7)
-    {
-        // returns a random direction (up, left, or right)
-        int choice = rand()%3;
-        if (choice == 0) return 'U';
-        else if (choice == 1) return 'L';
-        else if (choice == 2) return 'R';
-    }
-
-    else if (indexOfX == 8)
-    {
-        // returns a random direction (up or left)
-        int choice = rand()%2;
-        if (choice == 0) return 'U';
-        else if (choice == 1) return 'L';
-    }
-    return -1;                                                                  // should not return this
+    
+    return -1;                                                                         // should not return this
 }
 
 // prints the location of all numbers and the X in a (3x3) grid
-void printPuzzle(char positions[])
+void printPuzzle(char positions[3][3])
 {
-    cout << positions[0] << " " << positions[1] << " " << positions[2] << "\n"
-    << positions[3] << " " << positions[4] << " " << positions[5] << "\n"
-    << positions[6] << " " << positions[7] << " " << positions[8] << "\n" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << positions[i][0] << " " << positions[i][1] << " " << positions[i][2] << endl;
+    }
+    cout << endl;
 }
 
 // swaps the X with an adjacent number
-void move(char type, char positions[], int &indexOfX)
+void move(char type, char positions[3][3], int &XindexOfX, int &YindexOfX)
 {
-    int change;
-    int *pChange = &change;
+    int xchange = 0;
+    int ychange = 0;
+    int *pXchange = &xchange;
+    int *pYchange = &ychange;
 
     switch (type)
     {
-        case 'U': *pChange = -3;
+        case 'U': *pYchange = -1;
         break;
 
-        case 'D': *pChange = 3;
+        case 'D': *pYchange = 1;
         break;
 
-        case 'L': *pChange = -1;
+        case 'L': *pXchange = -1;
         break;
 
-        case 'R': *pChange = 1;
+        case 'R': *pXchange = 1;
     }
 
     // updates location of swapped number to where the X was
-    positions[indexOfX] = positions[indexOfX + change];
+    positions[YindexOfX][XindexOfX] = positions[YindexOfX + ychange][XindexOfX + xchange];
 
     // updates location of the X to where swapped number was
-    positions[indexOfX + change] = 'X';
+    positions[YindexOfX + ychange][XindexOfX + xchange] = 'X';
 
-    // updates indexOfX
-    indexOfX = indexOfX + change;
+    // updates values for location of X
+    XindexOfX = XindexOfX + xchange;
+    YindexOfX = YindexOfX + ychange;
 }
 
-int HammingDist(char positions[])
+// returns number of incorect values
+int HammingDist(char positions[3][3])
 {
     int heuristic = 0;
 
-    for (int i = 0; i < sizeof(&positions); i++)
+    // for each index...
+    for (int i = 0; i < 3; i++)
     {
-        if (positions[i] != '1' + i)
+        for (int j = 0; j < 3; j++)
         {
-            heuristic += 1;
+            // increments heuristic if value is not what it should be (unless it is X)
+            if (positions[i][j] != '1' + j + 3 * i && positions[i][j] != 'X')
+            {
+                heuristic += 1;
+            }
         }
     }
+    return heuristic;
+}
+
+// returns sum of all distances from correct value
+int ManhattanDist(char positions[3][3])
+{
+    int heuristic = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (positions[i][j] != 'X')
+            {
+                int x = (positions[i][j] - '0' - 1) % 3;
+                int y = (positions[i][j] - '0' - 1) / 3;
+
+                heuristic = heuristic + abs(x - j) + abs(y - i);
+            }
+        }
+    }
+
     return heuristic;
 }
